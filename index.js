3945ab9ab8c5284ef205898918a4386d91c7324a
Umair Jibran <umairjibran7@gmail.com>
2020-07-21 23:18:38 +0500

    understood eventemitter

--- index.js
+++ index.js
@@ -21,3 +21,16 @@ fs.readFile(
     else console.log(data);
   }
 );
+
+//Studying Event Emitter
+const EventEmitter = require("events");
+
+class Job extends EventEmitter {}
+
+let job = new Job();
+
+job.on("ushu", function (timeDone) {
+  console.log("Sta da kaar taqreeban ", timeDone, " ushu");
+});
+
+job.emit("ushu", new Date());
