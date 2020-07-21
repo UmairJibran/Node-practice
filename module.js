1d62a785c24d37b7f959187a045cd6eb5400f30f
Umair Jibran <umairjibran7@gmail.com>
2020-07-21 22:47:28 +0500

    exporting module as exports.methodName

--- module.js
+++ module.js
@@ -1,9 +1,7 @@
-let addNumbers = (numbersToAddUp) => {
+exports.sum = (numbersToAddUp) => {
   let sum = 0;
   for (let i = 0; i < numbersToAddUp.length; i++) {
     sum += numbersToAddUp[i];
   }
   return sum;
 };
-console.log("Hello from module.js");
-module.exports = { sum: addNumbers };
